d73566eaa1f3dea9ccc30d24e17ddeee47a566af
Rizqanmr <45690865+Rizqanmr@users.noreply.github.com>
2024-03-14 21:59:07 +0700

    move MainActivity to folder activities

--- app/src/main/java/com/rizqanmr/opengallery/activities/MainActivity.kt
+++ app/src/main/java/com/rizqanmr/opengallery/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rizqanmr.opengallery
+package com.rizqanmr.opengallery.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rizqanmr.opengallery.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
